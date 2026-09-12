294a1a8ae1290705e28053e172d306e43fe74d6f
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-13 02:00:23 +0600

    6. Product of Array Except Self

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -1,3 +1,33 @@
+
+
+
+// 06. Product of Array Except Self
+var productExceptSelf = function(nums) {
+
+    let result = [];
+    let product = 1;
+
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = product;
+        product *= nums[i];
+    }
+
+    product = 1;
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= product;
+        product *= nums[i];
+    }
+
+    return result;
+};
+
+console.log(productExceptSelf([1, 2, 3, 4]));
+
+
+
+
+
 // 05. Middle of the Linked List
 function ListNode(val, next) {
     this.val = (val === undefined ? 0 : val);
