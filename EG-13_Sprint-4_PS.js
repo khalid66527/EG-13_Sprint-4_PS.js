fd403f11913f194b093423312b5ce9c91dfb2ea6
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-13 02:03:38 +0600

    08. Find First and Last Position of Element in Sorted Array

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -1,4 +1,28 @@
 
+// 08. Find First and Last Position of Element in Sorted Array
+
+var searchRange = function(nums, target) {
+
+    let first = -1;
+    let last = -1;
+
+    for (let i = 0; i < nums.length; i++) {
+
+        if (nums[i] === target) {
+            if (first === -1) first = i;
+            last = i;
+        }
+    }
+
+    return [first, last];
+};
+
+
+
+
+
+
+
 // 07. Remove Nth Node From End of List
 
 var removeNthFromEnd = function(head, n) {
