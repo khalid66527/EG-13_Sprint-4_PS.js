e4471cd4bf93478c360c89ceaf9bddfcd82dd611
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-13 01:37:03 +0600

    02. Word Pattern

--- EG-13_Sprint-4_PS.js
+++ EG-13_Sprint-4_PS.js
@@ -1,5 +1,42 @@
 
 
+// 02. Word Pattern
+
+var wordPattern = function(pattern, s) {
+
+    let words = s.split(" ");
+
+    if (pattern.length !== words.length) return false;
+
+    let mapPW = {};
+    let mapWP = {};
+
+    for (let i = 0; i < pattern.length; i++) {
+
+        let char = pattern[i];
+        let word = words[i];
+
+        if (mapPW[char] && mapPW[char] !== word) {
+            return false;
+        }
+
+        if (mapWP[word] && mapWP[word] !== char) {
+            return false;
+        }
+
+        mapPW[char] = word;
+        mapWP[word] = char;
+
+    }
+
+    return true;
+};
+
+
+
+
+
+
 // 01. Isomorphic Strings
 var isIsomorphic = function(s, t) {
     if (s.length !== t.length) return false;
