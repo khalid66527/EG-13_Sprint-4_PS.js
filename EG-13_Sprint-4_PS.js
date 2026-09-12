fd3fdecb593b3884450b68eb02ef6c4052b69aae
khalid66527 <khalidhasan678954321@gmail.com>
2026-09-13 01:35:02 +0600

    01. Isomorphic Strings

---
+++ EG-13_Sprint-4_PS.js
@@ -0,0 +1,29 @@
+
+
+// 01. Isomorphic Strings
+var isIsomorphic = function(s, t) {
+    if (s.length !== t.length) return false;
+
+    let mapST = {};
+    let mapTS = {};
+
+    for (let i = 0; i < s.length; i++) {
+        let charS = s[i];
+        let charT = t[i];
+
+        if (mapST[charS] && mapST[charS] !== charT) {
+            return false;
+        }
+
+        if (mapTS[charT] && mapTS[charT] !== charS) {
+            return false;
+        }
+
+        mapST[charS] = charT;
+        mapTS[charT] = charS;
+    }
+
+    return true;
+};
+
+
